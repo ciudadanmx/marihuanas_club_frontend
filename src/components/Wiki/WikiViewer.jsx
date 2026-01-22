2cfe16f216cccce1e63dc7a2a4b7b58dbc7f8229
Ciudadan <ciudadanmx@gmail.com>
2026-01-22 07:12:46 -0600

    Se pasan las urls de las wikis a /utils/constants.js   en WikiViewer

--- src/components/Wiki/WikiViewer.jsx
+++ src/components/Wiki/WikiViewer.jsx
@@ -1,36 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import PreLoader from "../PreLoader";
 import "../../styles/WikiViewer.css";
+import { WIKI_BASE_URL, WIKI_ROUTES } from "../../utils/constants.js";
 
 const WikiViewer = () => {
   const [content, setContent] = useState("");
   const [loading, setLoading] = useState(true);
   const location = useLocation();
 
+  
+
   useEffect(() => {
     const fetchWiki = async () => {
       try {
-        let notionUrl = "";
-
         const path = window.location.pathname;
 
-        if (path === "/wiki/faq") {
-          notionUrl = "http://localhost:3033/wiki/2da8ae85d119808198fdfa1c2270ad59";
-        } else if (path === "/wiki/ayuda") {
-          notionUrl = "http://localhost:3033/wiki/2da8ae85d119804788d6dd577113c59a";
-        } else if (path === "/wiki/legal") {
-          notionUrl = "http://localhost:3033/wiki/2da8ae85d119804bbab2eccd3b57e174";
-        } else if (path === "/wiki/mapa420") {
-          notionUrl = "http://localhost:3033/wiki/2da8ae85d11980a98227eb5d8d7ed1b9";
-        } else {
-          notionUrl = "http://localhost:3033/wiki/2da8ae85d1198035990ecd7eb74a5701";
-        }
+        const pageId = WIKI_ROUTES[path] || WIKI_ROUTES.default;
+        const notionUrl = `${WIKI_BASE_URL}/${pageId}`;
 
         setLoading(true);
+
         const res = await fetch(notionUrl);
         if (!res.ok) throw new Error("Error al cargar la wiki");
-        let html = await res.text();
 
+        let html = await res.text();
         html = html.replace("<details>", "<details open>");
 
         setContent(html);
@@ -41,10 +35,11 @@ const WikiViewer = () => {
         setLoading(false);
       }
     };
+
     fetchWiki();
   }, [location.pathname]);
 
-  if (loading) return <p>Cargando contenido...</p>;
+  if (loading) return (<PreLoader />);
 
   const isMatrix = location.pathname === "/quienes-somos";
 
---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const WIKI_BASE_URL = "http://localhost:3033/wiki";
+
+export const WIKI_ROUTES = {
+  "/wiki/faq": "2da8ae85d119808198fdfa1c2270ad59",
+  "/wiki/ayuda": "2da8ae85d119804788d6dd577113c59a",
+  "/wiki/legal": "2da8ae85d119804bbab2eccd3b57e174",
+  "/wiki/mapa420": "2da8ae85d11980a98227eb5d8d7ed1b9",
+  default: "2da8ae85d1198035990ecd7eb74a5701",
+};
