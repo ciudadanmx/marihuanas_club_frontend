ccfb83d6128e0714ad46995bf717caeb89c3c2dd
Ciudadan <ciudadanmx@gmail.com>
2026-01-22 04:57:21 -0600

    Se agregan campos de habilidades del jardinero en paso de archivos de agregar club

--- src/components/Clubs/steps/Archivos.jsx
+++ src/components/Clubs/steps/Archivos.jsx
@@ -10,6 +10,7 @@ import {
   Checkbox,
   FormControlLabel,
   FormGroup,
+  MenuItem,
   Stack,
   FormHelperText,
 } from "@mui/material";
@@ -165,7 +166,7 @@ export default function Archivos({ form, setForm, tipo }) {
         />
       </Button>
 
-            {form?.foto_perfil && (
+      {form?.foto_perfil && (
         <Box mt={2}>
           <Typography
             variant="subtitle2"
@@ -223,98 +224,161 @@ export default function Archivos({ form, setForm, tipo }) {
       )}
 
 
+      {form.tipo_club?.includes("consumo") && (
+
+        <>
+        <Typography variant="h6" mt={4} mb={2}>
+          <u>📸 Fotos de Verificación de las áreas de cultivo:</u>
+        </Typography>
+
+        <Typography
+          variant="body1"
+          mb={2}
+        >
+          Toma en cuenta que los armarios de cultivo son de 1 x 1 m. y que requerirás pasillos por cada fila de armarios, por lo que en una zona de 3 x 3 caben hasta 7 plantas.
+          
+          Toma en cuenta también que uno de los armarios es para tu propio consumo, incluye también este armario e indica en total para cuántos armarios tienes espacio.
+          
+        </Typography>
+        
+
+
+        <TextField
+          select
+          label="Número de armarios"
+          fullWidth
+          name="armarios"
+          value={form.armarios || ""}
+          onChange={handleFormChange}
+          margin="normal"
+          color="success"
+          helperText="Número de armarios"
+          sx={{ width: 280 }}
+        >
+          {[...Array(11)].map((_, i) => {
+            const val = i + 2;
+            return (
+              <MenuItem key={val} value={val}>
+                {val}
+              </MenuItem>
+            );
+          })}
+        </TextField>
 
 
 
+        <Typography
+          variant="body1"
+          mb={2}
+          sx={{ fontStyle: "italic", color: "#59c054ff", fontSize: "0.85rem" }}
+        >
+          <strong>Sube al menos 5 fotos</strong>
+        </Typography>
 
-      <Typography variant="h6" mt={4} mb={2}>
-        <u>📸 Fotos de Verificación de las áreas de cultivo:</u>
-      </Typography>
-
-      <Typography
-        variant="body1"
-        mb={2}
-        sx={{ fontStyle: "italic", color: "#59c054ff", fontSize: "0.85rem" }}
-      >
-        <strong>Sube al menos 5 fotos</strong>
-      </Typography>
-
-      <Button
-        variant="contained"
-        component="label"
-        sx={{
-          backgroundColor: "#9c27b0",
-          "&:hover": { backgroundColor: "#7b1fa2" },
-        }}
-      >
-        ⬆️ Subir Fotos.
-        <Input
-          type="file"
-          name="documentales"
-          accept="image/*"
-          multiple
-          onChange={handleFileChange}
-          sx={{ display: "none" }}
-        />
-      </Button>
-
-            {form?.documentalesl && (
-        <Box mt={2}>
-          <Typography
-            variant="subtitle2"
-            sx={{ fontStyle: "italic", color: "#9c27b0", fontSize: "0.85rem" }}
-          >
-            Vista previa:
-          </Typography>
-          <img
-            src={URL.createObjectURL(form.documentales)}
-            alt="Vista previa fotos de verificación"
-            style={{
-              maxWidth: "100%",
-              maxHeight: "200px",
-              marginTop: "8px",
-              borderRadius: 8,
-            }}
+        <Button
+          variant="contained"
+          component="label"
+          sx={{
+            backgroundColor: "#9c27b0",
+            "&:hover": { backgroundColor: "#7b1fa2" },
+          }}
+        >
+          ⬆️ Subir Fotos.
+          <Input
+            type="file"
+            name="documentales"
+            accept="image/*"
+            multiple
+            onChange={handleFileChange}
+            sx={{ display: "none" }}
           />
-        </Box>
-      )}
+        </Button>
 
-      {form?.documentales?.length > 0 && (
-        <Grid container spacing={2} mt={2}>
-          {form.documentales.map((file, index) => (
-            <React.Fragment key={index}>
-              <Grid item xs={6} sm={4} md={3} position="relative">
-                <img
-                  src={URL.createObjectURL(file)}
-                  alt={`foto-${index}`}
-                  style={{
-                    width: "100%",
-                    borderRadius: 8,
-                    objectFit: "cover",
-                    height: "150px",
-                  }}
-                />
-                <button
-                  onClick={() => removeFotoClub(index)}
-                  style={{
-                    position: "absolute",
-                    top: 4,
-                    right: 4,
-                    background: "rgba(255,255,255,0.8)",
-                    border: "none",
-                    borderRadius: "50%",
-                    padding: 4,
-                    cursor: "pointer",
-                  }}
-                >
-                  <span style={{ color: "#d32f2f" }}>❌ Borrar</span>
-                </button>
-              </Grid>
-            </React.Fragment>
-          ))}
-        </Grid>
-      )}
+        {form?.documentales && (
+          <Box mt={2}>
+            <Typography
+              variant="subtitle2"
+              sx={{ fontStyle: "italic", color: "#9c27b0", fontSize: "0.85rem" }}
+            >
+              Vista previa:
+            </Typography>
+            <img
+              src={URL.createObjectURL(form.documentales)}
+              alt="Vista previa fotos de verificación"
+              style={{
+                maxWidth: "100%",
+                maxHeight: "200px",
+                marginTop: "8px",
+                borderRadius: 8,
+              }}
+            />
+          </Box>
+        )}
 
+        {form?.documentales?.length > 0 && (
+          <Grid container spacing={2} mt={2}>
+            {form.documentales.map((file, index) => (
+              <React.Fragment key={index}>
+                <Grid item xs={6} sm={4} md={3} position="relative">
+                  <img
+                    src={URL.createObjectURL(file)}
+                    alt={`foto-${index}`}
+                    style={{
+                      width: "100%",
+                      borderRadius: 8,
+                      objectFit: "cover",
+                      height: "150px",
+                    }}
+                  />
+                  <button
+                    onClick={() => removeFotoClub(index)}
+                    style={{
+                      position: "absolute",
+                      top: 4,
+                      right: 4,
+                      background: "rgba(255,255,255,0.8)",
+                      border: "none",
+                      borderRadius: "50%",
+                      padding: 4,
+                      cursor: "pointer",
+                    }}
+                  >
+                    <span style={{ color: "#d32f2f" }}>❌ Borrar</span>
+                  </button>
+                </Grid>
+              </React.Fragment>
+            ))}
+          </Grid>
+        )}
+
+
+        <Divider sx={{ my: 3, borderColor: "rgba(104, 64, 92, 0.57)" }} />
+      
+        <Typography variant="h6" mt={4} mb={2}>
+          <u>🌱 Habilidades Floristas:</u>
+        </Typography>
+        
+        <TextField
+          fullWidth
+          margin="normal"
+          label="Si tienes formación, experiencia, capacitaciones y habilidades como jardinero 4:20 descríbelas aquí..."
+          name="skills"
+          multiline
+          minRows={3}
+          value={form.skills || ""}
+          onChange={handleFormChange}
+          onFocus={() => setFocusedField("skills")}
+          onBlur={() => setFocusedField(null)}
+          color="success"
+          helperText={
+            focusedField === "skills"
+              ? "👉 Ingresa una descripción pública para tu Club."
+              : " "
+          }
+        />
+
+        </>
+      )}
 
 
 
